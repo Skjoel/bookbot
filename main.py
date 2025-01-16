f9c189469b52eceab281fe564b82af50aaa2fa72
Skjoel <56652656+Skjoel@users.noreply.github.com>
2025-01-16 15:28:39 +0100

    added charcter counting function

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     #print(text)
-    print(count_words(text))
+    print(f"There are {count_words(text)} words in the given text")
+    print(f"the following characters appear in the given text {count_characters(text)}")
 
 
 def get_book_text(path):
@@ -12,6 +13,18 @@ def get_book_text(path):
 def count_words(text):
     words = text.split()
     return len(words)
+
+def count_characters(text):
+    lowered_string = text.lower()
+    alphabet = {}
+    for a in lowered_string:
+        if a in alphabet:
+            alphabet[a] += 1
+        else:
+            alphabet[a] = 1
+    return alphabet
+    
+
 
 
 main()
