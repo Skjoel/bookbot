2c89eb141c5d3e5b657b4aaca2c7bde35b1080b9
Skjoel <56652656+Skjoel@users.noreply.github.com>
2025-01-16 15:02:05 +0100

    added threee functions to main

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def count_words(text):
+    counter = 0
+    while a in text:
+        if a != " ":
+            counter += 1
+        print(a)
+
+
+main()
